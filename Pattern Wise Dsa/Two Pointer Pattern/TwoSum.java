3125e3daf0dfae6d87fe77ccd7c741c22ca96a0f
Jatin <jlamba488@gmail.com>
2026-03-27 20:21:43 +0530

    two sum pattern added

---
+++ Pattern Wise Dsa/Two Pointer Pattern/TwoSum.java
@@ -0,0 +1,31 @@
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+
+            if (sum == target) {
+                return new int[]{i + 1, j + 1};
+            } else if (sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+
+    public static void main(String[] args) {
+        TwoSum obj = new TwoSum();
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = obj.twoSum(numbers, target);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
