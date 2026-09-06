0146dd9c89f484e1ef091830924197ebea81e9bc
Jatin Kumar <jlamba488@gmail.com>
2026-09-06 11:48:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countWays(int n) {
+        // code here
+        if(n<=2){
+            return n;
+        }
+        
+        return countWays(n-1)+countWays(n-2);
+    }
+}
